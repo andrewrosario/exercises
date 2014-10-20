c0035d6937302f98524929aa2119e164e190f0fb
Andy <rosario.andrew@gmail.com>
2014-10-20 18:40:50 -0500

    some things

--- Rock Paper Scissors/js.js
+++ Rock Paper Scissors/js.js
@@ -1,3 +1,5 @@
+// Rock Paper Scissors
+
     $('#scissors').on('click', function(){
        var result = compare('scissors', computerChoice());
        $("#decision").html(result);
@@ -60,4 +62,80 @@ var computerChoice = function() {
 	}
 };
 console.log(computerChoice);
+
+// Button Game
+
+  $("#name").on("change", function() {
+  	console.log("Working")
+    $(".name").append($("#name").val() +"'s")
+  });
+  
+
+
+  $(document).on("click", ".button", function() { 
+  	var button = $("<button>").addClass('button').html('click me');
+  	$(this).after(button)
+  	$(".score").replaceWith("<span class='score'>" + $('.button').length + "</span>")
+  });
+
+
+  $(document).on("dblclick", ".button", function() { 
+  	$(".button").slice(-3).remove()
+  	$(".score").replaceWith("<span class='score'>" + $('.button').length + "</span>")
+  });
+
+  $(document).on("click", ".start", function() {
+
+
+// Counter	
+var i = 0
+	setInterval(function(){
+		
+		if (i<= 100) {$(".counter").replaceWith("<p class='counter'>" + i + "</p>"); i++};
+
+	}, 100);		 
+  });
+
+
+// Nested Loops and 2D arrays
+
+// Create an array of arrays, aka a 2D array. Using the example listed below.
+// Sample array : var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+// Sample Output :
+// "row 0"
+// " 1"
+// " 2"
+// " 1"
+// " 24"
+// "row 1"
+// etc....
+
+//***********To access 2-dimensional array elements we would "console.log(array[i][j]);"
+
+var numberSet = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+
+//Write the first for loop
+for(i=0; i <= 4; i++){
+	
+	for(j = 0; j <= 3; j++){ 
+		console.log(numberSet[i][j]);
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
